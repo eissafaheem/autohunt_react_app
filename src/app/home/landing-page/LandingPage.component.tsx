95eec298bbb13192bd67ce6f0d8541a23cd4b4c9
Eissa Faheem <eissafaheem@gmail.com>
2023-08-14 17:51:14 +0530

    Feat: Scrollbar customized

--- src/app/home/landing-page/LandingPage.component.tsx
+++ src/app/home/landing-page/LandingPage.component.tsx
@@ -4,10 +4,15 @@ import LandingPageStyles from './LandingPage.module.css'
 function LandingPageComponent() {
   return (
     <div className={LandingPageStyles["main-container"]}>
-      <div className={LandingPageStyles["tagline"]}>
-       <h1>Find your dream car</h1>
-       <p>Discover Your Dream Drive <br /> From Search to Street, All Online</p>
-      </div>
+      <section className={LandingPageStyles['welcome']}>
+        <div className={LandingPageStyles["tagline"]}>
+          <h1>Find your dream car</h1>
+          <p>Discover Your Dream Drive <br /> From Search to Street, All Online</p>
+        </div>
+      </section>
+      <section className={LandingPageStyles['recomended']}>
+        l
+      </section>
     </div>
   )
 }
